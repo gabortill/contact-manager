6520665d47cab6defb8093a9ad22fefcea4f8118
tillgabi <till.gabor@outlook.com>
2015-04-18 18:11:34 +0200

    flipping card

--- development/js/directives/businessCard.directive.js
+++ development/js/directives/businessCard.directive.js
@@ -3,7 +3,11 @@
 
 	function businessCard() {
 		return {
-			link: function (scope, elem) {},
+			link: function (scope, elem) {
+				elem.on('click', function () {
+					elem[0].childNodes[0].classList.toggle('card__flipper--inverse');
+				});
+			},
 			replace: true,
 			restrict: 'E',
 			templateUrl: 'directive.businessCard.tpl.html'
